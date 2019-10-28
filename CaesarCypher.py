bffc8f0787c1459bf0385e9597f1ab98634981f6
DpalmeITESM <a01747290@itesm.mx>
2019-10-28 11:47:49 -0600

    Code y Decode red

--- CaesarCypher.py
+++ CaesarCypher.py
@@ -1,12 +1,7 @@
 def encode(key, stringToEncode):
-    print(stringToEncode)
     finalString = ""
     for char in stringToEncode.lower():
-        if char.isalpha():
-            finalString += chr(ord(char) + key - (ord("z")-ord("a")+1) if ord(char)+key > ord("z") else ord(char) + key)
-        else:
-            finalString+=char
-    print(finalString)
+            finalString += chr(ord(char) + key - (ord("z")-ord("a")+1) if ord(char)+key > ord("z") else ord(char) + key) if char.isalpha() else char
     return finalString
 
 
@@ -14,15 +9,5 @@ def decode(key, mensaje):
     mensajeDescifrado = ""
     for palabra in mensaje.lower():
         for letra in palabra:
-            if letra.isalpha():
-                x = ord(letra)
-                if x - key < 97:
-                    x = 122 - key + (x - 96)
-                else:
-                    x -= key
-                mensajeDescifrado += chr(x)
-            else:
-                mensajeDescifrado += letra
+                mensajeDescifrado += chr(122 - key + ord(letra) - 96 if ord(letra) - key < 97 else ord(letra) - key) if letra.isalpha() else letra
     return mensajeDescifrado
-
-print(decode(24, encode(24, "Que pedo soy el mero mero miguel hidalgo hdtptm. viaje en el tiempo para partirte tu reputisima madre")))
