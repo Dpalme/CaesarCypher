d417412b5ca80aedcaa0b3a9685151efdd6255e0
DpalmeITESM <a01747290@itesm.mx>
2019-10-30 11:33:46 -0600

    caesar final

--- CaesarCypher.py
+++ CaesarCypher.py
@@ -78,11 +78,9 @@ def keyFinder(mensaje):
     return keyWithChi(Letras, contador)
 
 key = random.randint(0,26)
-# key = 0
 originalMessage="Las autoridades han informado que las fuertes lluvias se continuarán presentando en los próximos días en diversas zonas de la CDMX. Además, se prevé que la temperatura disminuya, debido a la entrada del frente frío número 7. Una de las alcaldías que más afectaciones ha presentado debido a las fuertes precipitaciones es Iztapalapa, donde las inundaciones alcanzan hasta los 60 centímetros. De acuerdo con Excélsior, otras alcaldías que han sido afectadas son Azcapotzalco, Álvaro Obregón, Cuauhtémoc, Gustavo A. Madero y Miguel Hidalgo, aunque sólo con leves daños materiales y, eso sí, serios detrimentos al tránsito."
+originalMessage = normalizeString(originalMessage)
 codedMessage = encode(key, originalMessage)
 print(originalMessage)
-print(key)
 print(codedMessage)
-print(keyFinder(codedMessage))
 print(decode(keyFinder(codedMessage), codedMessage))
